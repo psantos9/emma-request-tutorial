bca40d98149988dcd2c4881f1780535aeb56e0d6
Paulo Santos <paulo.santos@leanix.net>
2023-07-24 11:39:06 -0300

    wip

--- src/businessLogic.spec.ts
+++ src/businessLogic.spec.ts
@@ -1,25 +1,13 @@
-import { Authenticator, GraphQLClient } from 'leanix-js'
-import { fetchApplications, mutateApplications } from './businessLogic'
+import { sum, getQuote } from './businessLogic'
 
-const host = process.env.LEANIX_HOST
-const apitoken = process.env.LEANIX_APITOKEN
+it('computes the sum of two numbers', async () => {
+  const a = 1
+  const b = 2
+  const c = sum(1, 2)
+  expect(c).toBe(a + b)
+})
 
-describe('The business logic', () => {
-  const authenticator = new Authenticator({ host, apitoken })
-  const graphql = new GraphQLClient(authenticator)
-  beforeAll(async () => {
-    expect(typeof host).toEqual('string')
-    expect(typeof apitoken).toEqual('string')
-    await authenticator.start()
-  })
-  afterAll(() => {
-    authenticator.stop()
-  })
-
-  it('fetches applications from the inventory', async () => {
-    const applications = await fetchApplications({ graphql })
-    expect(Array.isArray(applications)).toBeTruthy()
-    const updatedApplications = await mutateApplications({ applications, graphql })
-    expect(Array.isArray(updatedApplications)).toBeTruthy()
-  }, 10000)
+it('gets a quote from an external api', async () => {
+  const quote = await getQuote()
+  expect(typeof quote).toBe('string')
 })
